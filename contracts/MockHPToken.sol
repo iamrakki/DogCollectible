4022e5ae16cd1746a4b8a0601757501c799ea709
Rakki <rakkiramesh4@gmail.com>
2025-05-12 18:53:12 +0530

    Add:MockToken

---
+++ contracts/MockHPToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockHPToken is ERC20 {
+    constructor() ERC20("Happiness Token", "HP") {
+        _mint(msg.sender, 1000000000 * 10**18);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
