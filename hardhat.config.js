65c10595f4065ab8afa484a44dbd4d0b221b7779
Rakki <rakkiramesh4@gmail.com>
2025-05-12 16:40:38 +0530

    Update package

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,34 @@
 require('@nomiclabs/hardhat-ethers');
+require("hardhat-contract-sizer");
+require("@nomicfoundation/hardhat-chai-matchers");
+
+
+
+require('dotenv').config();
+
+const { PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.28",
-  networks: {
-    hardhat: {},
-    xdc: {
-      url: `https://rpc.apothem.network`,
-      accounts: [""]
-    },
-  },
   settings: {
     optimizer: {
       enabled: true,
       runs: 200, 
     },
   },
+  networks: {
+    hardhat: {},
+    xdc: {
+      url: `https://rpc.apothem.network`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+    only: [],
+  },
+
 };
