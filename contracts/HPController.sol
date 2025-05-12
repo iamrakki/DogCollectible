15d2f7e66c425df2d548a60e4ff9db59fb3a9e55
Rakki <rakkiramesh4@gmail.com>
2025-05-12 18:48:05 +0530

    Import:Token

--- contracts/HPController.sol
+++ contracts/HPController.sol
@@ -4,17 +4,27 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract HPController is AccessControl {
+    using SafeERC20 for IERC20;
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
-    mapping(address => uint256) public userHP;
+    IERC20 public immutable hpToken;
     uint256 public baseHPCost = 8000;
     uint256 public bulkDiscountRate = 10;
 
-    constructor() {
+    event HPDeducted(address indexed user, uint256 amount);
+    event HPAdded(address indexed user, uint256 amount);
+    event HPWithdrawn(address indexed admin, uint256 amount);
+
+    constructor(address _hpToken) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
+        hpToken = IERC20(_hpToken);
     }
 
     function setBaseHPCost(uint256 _cost) external onlyRole(ADMIN_ROLE) {
@@ -27,34 +37,40 @@ contract HPController is AccessControl {
     }
 
     function calculateHP(uint256 quantity) public view returns (uint256) {
-    return baseHPCost * quantity;
-}
+        return baseHPCost * quantity;
+    }
 
-function calculateDiscountedHP(uint256 quantity) public view returns (uint256) {
-    uint256 discount = bulkDiscountRate;
-    require(discount <= 100, "Invalid discount rate");
-    uint256 totalCost = baseHPCost * quantity * (100 - discount) / 100;
-    return totalCost;
-}
+    function calculateDiscountedHP(uint256 quantity) public view returns (uint256) {
+        uint256 discount = bulkDiscountRate;
+        require(discount <= 100, "Invalid discount rate");
+        uint256 totalCost = baseHPCost * quantity * (100 - discount) / 100;
+        return totalCost;
+    }
 
     function deductHP(address user, uint256 amount) external returns (bool) {
-        if (userHP[user] >= amount) {
-            userHP[user] -= amount;
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not authorized");
+        if (hpToken.balanceOf(user) >= amount) {
+            require(hpToken.allowance(user, msg.sender) >= amount, "Insufficient allowance");
+            hpToken.safeTransferFrom(user, address(this), amount);
+            emit HPDeducted(user, amount);
             return true;
         }
         return false;
     }
 
     function addHP(address user, uint256 amount) external onlyRole(ADMIN_ROLE) {
-        userHP[user] += amount;
-    }
-
-    function removeHP(address user, uint256 amount) external onlyRole(ADMIN_ROLE) {
-        require(userHP[user] >= amount, "Insufficient HP");
-        userHP[user] -= amount;
+        hpToken.safeTransfer(user, amount);
+        emit HPAdded(user, amount);
     }
 
     function getUserHP(address user) external view returns (uint256) {
-        return userHP[user];
+        return hpToken.balanceOf(user);
     }
+
+    function withdrawHP(uint256 amount) external onlyRole(ADMIN_ROLE) {
+        require(hpToken.balanceOf(address(this)) >= amount, "Insufficient balance");
+        hpToken.safeTransfer(msg.sender, amount);
+        emit HPWithdrawn(msg.sender, amount);
+    }
+
 }
